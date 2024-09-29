2331563dd71ea16368720cbc96baebf7a8a7d6a9
DuyDo1007 <ddduy7890@gmail.com>
2024-09-29 18:51:46 +0700

    final commit

--- ChoosingOption.java
+++ ChoosingOption.java
@@ -2,8 +2,8 @@
 import javax.swing.JOptionPane;
 public class ChoosingOption {
     public static void main(String[] args){
-    int Option = jOPtionPane.showConfirmDialog(null,"Do you want to change the first class ticket?");
-    JOptionPane.showMessageDialog(null,"You've chosen:"+(option == jOptionPane.YES_OPTION?"Yes":"No"));
+    int Option = JOptionPane.showConfirmDialog(null,"Do you want to change the first class ticket?");
+    JOptionPane.showMessageDialog(null,"You've chosen:"+(Option == JOptionPane.YES_OPTION?"Yes":"No"));
     System.exit(0);
 }
 }
