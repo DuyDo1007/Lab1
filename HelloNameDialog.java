1600909591079a71fd415ff6c61ac0b2fe30ce49
DuyDo1007 <165807434+DuyDo1007@users.noreply.github.com>
2024-09-29 19:00:08 +0700

    Update HelloNameDialog.java

--- HelloNameDialog.java
+++ HelloNameDialog.java
@@ -3,6 +3,6 @@ public class HelloNameDialog {
 public static void main(String[] args) { 
     String result;
     result = JOptionPane.showInputDialog("Please enter your name:"); 
-    JOptionPane.showMessageDialog(null, "Hi "+ result + "!"); 
+    JOptionPane.showMessageDialog(null, "Hi "+ result + "!");
 } 
 }
